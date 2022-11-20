21fb7a7b731e07682e80a30886b48cb169ed50ee
SakethSaiNigam <118318675+SakethSaiNigam@users.noreply.github.com>
2022-11-20 17:37:23 +0000

    This is Practical 2

---
+++ prac2/Sum.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+int main(void) {
+/* Declare variables */
+   int i;
+   float sum1, sum2, diff;
+   
+
+/* First sum */
+   sum1 = 0.0;
+   for (i=1; i<=1000; i++) {
+      sum1 =sum1+ (1/(float)i);
+   }
+
+
+/* Second sum */
+   float x;
+   sum2 = 0.0;
+   for (i=1000; i>0; i--) {
+     x = i;
+     sum2 =sum2 + (1/x);
+   }
+
+   printf(" Sum1=%f\n",sum1);
+   printf(" Sum2=%f\n",sum2);
+
+/* Find the difference */
+   diff = sum1-sum2;
+
+   printf(" Difference between the two is %f\n",diff);
+
+}
+
+
+
